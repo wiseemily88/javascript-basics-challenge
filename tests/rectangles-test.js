ecc6aa9da67d944dddfc490b0dd8eee705e6cbc8
Emily Wise <wise.m.emily@gmail.com>
2018-01-22 12:09:59 -0700

    Passing area

--- tests/rectangles-test.js
+++ tests/rectangles-test.js
@@ -1,1 +1,11 @@
 assert = require('chai').assert;
+const area = require('../lib/rectangles')
+describe ("rectangle properties", function() {
+
+  it("the area function returns area", function(){
+    var rectangle = {width: 10, height: 20}
+
+    assert.strictEqual(area(rectangle), 200)
+
+  })
+})
