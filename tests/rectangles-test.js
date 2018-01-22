1d47806ac19daefebc9c3cd47a1469f53bf96c4b
Emily Wise <wise.m.emily@gmail.com>
2018-01-22 12:17:18 -0700

    add perimeter

--- tests/rectangles-test.js
+++ tests/rectangles-test.js
@@ -1,11 +1,15 @@
 assert = require('chai').assert;
-const area = require('../lib/rectangles')
+const myFunctions = require('../lib/rectangles')
 describe ("rectangle properties", function() {
 
   it("the area function returns area", function(){
     var rectangle = {width: 10, height: 20}
+    assert.strictEqual(myFunctions.area(rectangle), 200)
 
-    assert.strictEqual(area(rectangle), 200)
+  })
 
+  it("the perimeter function returns perimeter", function(){
+    var rectangle = {width: 10, height: 20}
+    assert.strictEqual(myFunctions.perimeter(rectangle), 60)
   })
 })
